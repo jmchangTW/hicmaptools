f56eec8081a09b903e8213de4ed5c864efde78ed
jia-ming.chang <chang.jiaming@gmail.com>
2021-05-23 15:29:18 +0800

    add docs/README.rst & tools/visualizeRandomTest.R

--- tools/visualizeRandomTest.R
+++ tools/visualizeRandomTest.R
@@ -1,12 +1,11 @@
 library(ggplot2)
+library(nortest)
 
 args = commandArgs(trailingOnly=TRUE)
 
 if (length(args)==0 || length(args)==1) {
         stop("USAGE: Rscript normality_test.R InputFile OutputName", call.=FALSE)
 }else if (length(args)==2) {
-    #cat("Enter output filename: ")
-    #filename <- scan("stdin", character(), n=1)
     mydata = read.table(args[1],header = TRUE,sep = ",")
     temp <- args[2]
     g <- gregexpr("/", temp, fixed=TRUE)
@@ -42,29 +41,17 @@ if(result$p.value<0.05){
 }else{
     message("According to Pearson chi-square TEST, we do not have evidence to reject the null hypothesis that the sample is normal distribution\n");
 }
+
 pdf(paste(path,filename,".pdf"))
 
-qplot(x=mydata$random_obs,
-data = mydata,
-col = "red",
-geom="density",
-xlab="obs",
-)+theme(legend.position="none")+geom_vline(xintercept = mydata[1,1])+ggtitle(paste0("Densityplot of random obs,\nquery obs =",mydata[1,1])) +theme(plot.title = element_text(hjust = 0.5))
-
-
-qplot(x=mydata$random_exp,
-data = mydata,
-col = "red",
-geom="density",
-xlab="exp",
-)+theme(legend.position="none")+geom_vline(xintercept = mydata[1,2])+ggtitle(paste0("Densityplot of random exp,\nquery exp =",mydata[1,2])) +theme(plot.title = element_text(hjust = 0.5))
-
-
-qplot(x=mydata$random_nor,
-data = mydata,
-col = "red",
-geom="density",
-xlab="exp",
-)+theme(legend.position="none")+geom_vline(xintercept = mydata[1,3])+ggtitle(paste0("Densityplot of random nor,\nquery nor =",mydata[1,3])) +theme(plot.title = element_text(hjust = 0.5))
+qplot(random_nor,
+      data = mydata,
+      col = "red",
+      geom="density",
+      xlab="score",)+
+  theme(legend.position="none")+
+  geom_vline(xintercept = mydata[1,3])+
+  ggtitle(paste0("Densityplot of random nor,\n query nor =",mydata[1,3]))+
+  theme(plot.title = element_text(hjust = 0.5))
 
 a <- dev.off()
